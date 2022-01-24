2b38026ae80a28c480d0a8f02e943df61267a6e6
Kanaha150 <71273013+Kanaha150@users.noreply.github.com>
2022-01-24 17:21:20 +0100

    Add files via upload

--- tempRhSensor.js
+++ tempRhSensor.js
@@ -5,9 +5,10 @@ const Generic_Access_Primary_Service = 0x1800;
 const Generic_Attribute_Primary_Service = 0x1801;
 const Device_Information_Primary_Service = 0x180A;
 const Battery_Service_Primary_Service = 0x180F;
-const Unknown_Service_Primary_Service_a = '0000f234-b38d-4985-720e-0f993a68ee41'; // same as Logger serice UUID
-const Unknown_Service_Primary_Service_b = '00001234-b38d-4985-720e-0f993a68ee41'; //same as RH service UUID
-const Unknown_Service_Primary_Service_c = '00002234-b38d-4985-720e-0f993a68ee41'; // same as TEmp service UUID
+const Unknown_Service_Primary_Service_1 = '0000f234-b38d-4985-720e-0f993a68ee41'; // same as Logger serice UUID
+const Unknown_Service_Primary_Service_2 = '00001234-b38d-4985-720e-0f993a68ee41'; //same as RH service UUID
+const Unknown_Service_Primary_Service_3 = '00002234-b38d-4985-720e-0f993a68ee41'; // same as Temp service UUID
+
 
     // Custom Bluetooth service UUID
     const SENSIRION_DEVICE_INFO_SERVICE_UUID = 0x2800;// 180A; // 0x02010//0x180A;
@@ -24,6 +25,8 @@ const Unknown_Service_Primary_Service_c = '00002234-b38d-4985-720e-0f993a68ee41'
     const LoggerIntervalMs_UUID = 0xF239; //0000F239-B38D-4985-720E-0F993A68EE41;
 
     const SENSIRION_TEMP_UUID =0x2235; //00001235-B38D-4985-720E-0F993A68EE41; or maybe 0x1235????
+    const Temp_test_UUID_1 = 0x2901;
+    const Temp_test_UUID_2 = 0x2902;
     const SENSIRION_RH_UUID = 0x1235; //00002235-B38D-4985-720E-0F993A68EE41; or maybe 0x2235???????
 
 
@@ -37,7 +40,7 @@ const Unknown_Service_Primary_Service_c = '00002234-b38d-4985-720e-0f993a68ee41'
             return navigator.bluetooth.requestDevice({ 
                 filters: [{ 
                     name: 'Smart Humigadget'}],
-                optionalServices: [0x1800] 
+                // optionalServices: [Generic_Access_Primary_Service] 
             })
                 .then(device => {
                     this.device = device;
@@ -48,7 +51,7 @@ const Unknown_Service_Primary_Service_c = '00002234-b38d-4985-720e-0f993a68ee41'
                     return server.getPrimaryService(SENSIRION_TEMP_SERVICE_UUID);
                 })
                 .then(service => {
-                    return this._cacheCharacteristic(service, SENSIRION_TEMP_UUID);
+                    return this._cacheCharacteristic(service, Temp_test_UUID_1);
                 })
                 .then(value => {
                     console.log(`Temp is ${value.getUint8(0)}`);
@@ -58,10 +61,10 @@ const Unknown_Service_Primary_Service_c = '00002234-b38d-4985-720e-0f993a68ee41'
         /* Temp Service */
 
         startNotificationsTempRhMeasurement() {
-            return this._startNotifications(SENSIRION_TEMP_UUID);
+            return this._startNotifications(Temp_test_UUID_1);
         }
         stopNotificationsTempRhMeasurement() {
-            return this._stopNotifications(SENSIRION_TEMP_UUID);
+            return this._stopNotifications(Temp_test_UUID_1);
         }
         parseTempRh(value) {
             // In Chrome 50+, a DataView is returned instead of an ArrayBuffer.
